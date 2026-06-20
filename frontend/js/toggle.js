e7b8fc31621180eaa9c8e4fd0c977ae10ae4f71f
Manasa <24wh1a05c7@bvrithyderabad.edu.in>
2026-06-20 10:21:46 +0530

    toggle

--- frontend/js/toggle.js
+++ frontend/js/toggle.js
@@ -1,27 +1,22 @@
-toggle.js
 var users = [
-    {
-        "name" : "John doe",
-        "gender" : "male",
-        "image" : "/images/john.png"
-
-    },
-    {
-        "name" : "Jane",
-        "gender" : "female",
-        "image" : "/images/jane.png"
-    }
+ {
+    "name" : "John Doe",
+    "gender" : "male",
+    "image" : "/images/john.png"
+ },
+ {
+    "name" : "Jane Doe",
+    "gender": "female",
+    "image" : "/images/jane.png"
+ }
 ]
-
-var curId = 0;
-
+var curId = 0; 
 function toggle(){
-    //toggle curId from 0 -> 1 & 1 -> 0
-    curId = (curId + 1) % 2;
-
+    curId+=1;
+    curId%=2;
+    //toggle the rendered elements
     var user = users[curId];
-    document.getElementById("user-img").src=user.image;
-    document.getElementById("user-name").innerText = user.name;
-    document.getElementById("user-gender").innerText = user.gender;
-
+    document.getElementById("userimg").src=user.image;
+    document.getElementById("username").innerText=user.name;
+    document.getElementById("usergender").innerText=user.gender;
 }
